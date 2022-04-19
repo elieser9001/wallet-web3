8bd1023cbe0f8db36dcc9f20b5863d050c9025fc
Elieser Reyes <platalunasol@gmail.com>
2022-04-19 00:33:21 -0500

    dist folder removed

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,7 +2,7 @@ const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"
 const { HotModuleReplacementPlugin } = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-const outputPath = "../dist";
+const outputPath = "../dist/static";
 
 /** @type {import('webpack').Configuration} */
 const devConfig = {
